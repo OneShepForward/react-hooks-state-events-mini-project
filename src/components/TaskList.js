ed2c49657b16de21c9aad8c49ad2395deeda2bcb
Nicholas Shephard <shephardn@gmail.com>
2021-11-30 21:47:46 -0600

    The whole thing works, but the tests won't accept it.

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,10 +1,40 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList({ tasks, categories, onDelete, currentCategory }) {
+
+
+function renderTasks() {
+    const filteredTasks = tasks.filter(task => {
+    if (currentCategory === "All") {
+      return task;
+    } else {
+      return task.category === currentCategory;
+    }
+  })
+  
+  return filteredTasks.map(task => {
+    return (<Task 
+              text={task.text} 
+              category={task.category} 
+              key={task.text} 
+              onDelete={onDelete}
+              className="task" 
+            />
+    );
+  })
+
+}
+let taskClassNames = "tasks task"
+
+// console.log(tasks);
   return (
-    <div className="tasks">
+    <>
+    <div className={taskClassNames}>
       {/* display a list of tasks using Task component */}
+      {renderTasks()}
     </div>
+    </>
   );
 }
 
